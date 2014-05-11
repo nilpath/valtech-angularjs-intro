5a91416bb27030a9155f09ea47b1c8d54f668881
Christoffer Åhrling <christoffer@nilpath.se>
2014-05-11 15:23:54 +0200

    updated the solution for part one

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,7 @@ angular.module('valtechAngularIntroApp', [])
 
     $scope.list = [
       {
+        id: 0,
         qty: 1,
         unit: 'dl',
         product: 'Matlagningsgrädde',
@@ -16,6 +17,7 @@ angular.module('valtechAngularIntroApp', [])
         purchased: true
       },
       {
+        id: 1,
         qty: 4,
         unit: 'st',
         product: 'Äpplen',
@@ -23,6 +25,7 @@ angular.module('valtechAngularIntroApp', [])
         purchased: true
       },
       {
+        id: 2,
         qty: 1,
         unit: 'påse',
         product: 'bröd',
@@ -30,6 +33,7 @@ angular.module('valtechAngularIntroApp', [])
         purchased: false
       },
       {
+        id: 3,
         qty: 5,
         unit: 'st',
         product: 'röda paprikor',
@@ -37,6 +41,7 @@ angular.module('valtechAngularIntroApp', [])
         purchased: false
       },
       {
+        id: 4,
         qty: 1,
         unit: 'st',
         product: 'Magnum Mandel',
@@ -49,12 +54,50 @@ angular.module('valtechAngularIntroApp', [])
       formType: FORM_TYPES[0]
     };
 
+    function clearForm() {
+      $scope.formItem = {
+        formType: FORM_TYPES[0]
+      };
+    }
 
     $scope.handleItem = function() {
       var item = $scope.formItem;
       if(item.formType === FORM_TYPES[0]) {
         item.purchased = false;
         $scope.list.push(item);
+        clearForm();
       }
+      else if(item.formType === FORM_TYPES[1]) {
+        angular.forEach($scope.list, function(itm){
+          if(itm.id === item.id) {
+            itm.qty = item.qty;
+            itm.unit = item.unit;
+            itm.product = item.product;
+            itm.price = item.price;
+            clearForm();
+          }
+        });
+      }
+
     };
+
+    $scope.editItem = function(item, $event) {
+      $event.preventDefault();
+      $scope.formItem = angular.copy(item);
+      $scope.formItem.price = Number($scope.formItem.price);
+      $scope.formItem.formType = FORM_TYPES[1];
+    };
+
+    $scope.deleteItem = function(item, $event) {
+      $event.preventDefault();
+      var index;
+      angular.forEach($scope.list, function(itm, idx){
+        if(itm.id === item.id) {
+          index = idx;
+        }
+      });
+
+      $scope.list.splice(index, 1);
+    };
+
   }]);
